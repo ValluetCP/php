c3a57bbab39abf314d3a0047278dfb17aa421b13
CYNTHIA <cynthia.petitot@gmail.com>
2023-08-18 16:37:20 +0200

    TP-PHP

--- projet/form.php
+++ projet/form.php
@@ -13,18 +13,70 @@
 <body>
     <h1>Formulaire PHP</h1>
     <div class="container">
-        <form action="" method="">
+        <form action="./traitement/actionForm.php" method="POST">
+            <h2>Informations</h2>
+            <label for="civility">Civilité</label><br>
+            <Select name="civility">
+                <option value="monsieur">monsieur</option>
+                <option value="madame">madame</option>
+            </Select><br>
+
             <label for="firstname">First name</label><br>
-            <input type="text" name="firstname" id="firstname" placeholder=" Enter your firstname" required><br>
+            <input class="champs" type="text" name="firstname" id="firstname" placeholder=" Enter your firstname" required><br>
     
             <label for="lastname">Last name</label><br>
-            <input type="text" name="lastname" id="lastname" placeholder=" Enter your lastname" required><br>
+            <input class="champs" type="text" name="lastname" id="lastname" placeholder=" Enter your lastname" required><br>
     
             <label for="email">Email</label><br>
-            <input type="email" name="email" id="email" placeholder=" Enter your email" required><br>
-    
-            <input type="submit" name="submit" id="submit" value="Submit">
+            <input class="champs" type="email" name="email" id="email" placeholder=" Enter your email" required><br>
+
+            <label for="password">Mot de passe</label><br>
+            <input class="champs" type="password" name="password" id="password" placeholder=" Enter your password" required><br>
+
+            <div class="flexy numb">
+                <div>
+                    <label for="birthday">Date d'anniversaire</label><br>
+                    <input class="champs2" type="birthday" name="birthday" id="birthday" placeholder=" JJ/MM/AAAA" required><br>
+                </div>
+                <div>
+                    <label class="number" for="phone">Numéro de téléphone</label><br>
+                    <input class="champs2" type="phone" name="phone" id="phone" placeholder=" +33 "required><br>
+                </div>
+            </div>
+
+            <h2>Destination</h2>
+            <fieldset class="radio">
+                <p>Veuillez sélectionner un pays</p>
+                <input type="radio" id="france" name="country" value="france">
+                <label for="html">France</label><br>
+
+                <input type="radio" id="espagne" name="country" value="espagne">
+                <label for="espagne">Espagne</label><br>
+
+                <input type="radio" id="allemagne" name="country" value="allemagne">
+                <label for="allemagne">Allemagne</label>
+            </fieldset>
+
+            <p class="com">Laisser un commentaire</p>
+            <textarea name="message" id="message" cols="30" rows="10" style="width: 635px; height: 194px;">Commentaire</textarea><br>
+
+            <input type="checkbox" class="checkbox" name="conditions">
+            <label for="conditions" class="condition">conditions générales de ventes</label><br>
+
+            <input class="champs" type="submit" name="submit" id="submit" value="Envoyer">
         </form>
     </div>
+    <!-- C'est PHP qui sert d'intermédiaire pour communiquer et interagir avec la base de donnée -->
+    <!-- PHP est intermédiaire
+    SGBD : serveur de bdd
+
+    Les 6 éléments pour interagir avec la bdd
+        1-le client (navigateur)
+        2- HTML
+        3- APACHE (serveur pour interpréter le langage PHP)
+        4- PHP
+        5-SGBD (serveur bdd)
+        6- requête (langage mySQL)
+-->
 </body>
 </html>
